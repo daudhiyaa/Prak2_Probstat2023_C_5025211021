a7f9f2f4af7fd6ec925fd0c51dd4ebff65aabafa
Daud Dhiya <daud.dhiya31@gmail.com>
2023-05-31 21:04:32 +0700

    feat: add all answer

---
+++ Prak2_Probstat2023_5025211021.R
@@ -0,0 +1,67 @@
+# No 1
+  X <- c(78, 75, 67, 77, 70, 72, 78, 70, 77)
+  Y <- c(100, 95, 70, 90, 90,   90, 89, 100, 100)
+  
+  # Poin A
+  difference <- Y - X
+  sd(difference)
+  
+  # Poin B
+  t.test(Y, X, paired = TRUE)
+  
+  # Poin C
+  # Jawaban terlampir di README.md
+
+# No 2
+  # Poin A & C
+  # Jawaban terlampir di README.md
+
+# No 3
+  # Poin A
+  # Jawaban terlampir di README.md
+  
+  # Poin B
+  library(BSDA)
+  tsum.test(mean.x=3.64, s.x = 1.67, n.x = 20, 
+            mean.y =2.79 , s.y = 1.5, n.y = 27, 
+            alternative = "greater", var.equal = TRUE)
+  
+  # Poin C
+  library(mosaic)
+  plotDist(dist = 't', df = 2, col = "red")
+  
+  # Poin D
+  qchisq(p = 0.05, df = 2, lower.tail = FALSE)
+  
+  # Poin E & F
+  # Jawaban terlampir di README.md
+  
+# Soal 5
+  install.packages("multcompView")
+  library(readr)
+  library(ggplot2)
+  library(multcompView)
+  library(dplyr)
+  
+  # Poin A
+  nama_file <- "GTL.csv"
+  GTL <- read_csv(nama_file)
+  head(GTL)
+  
+  str(GTL)
+  
+  qplot(x = Temp, y = Light, geom = "point", data = GTL) + facet_grid(.~Glass, labeller = label_both)
+  
+  # Poin B
+  GTL$Glass <- as.factor(GTL$Glass)
+  GTL$Temp_Factor <- as.factor(GTL$Temp)
+  str(GTL)
+  
+  anova <- aov(Light ~ Glass*Temp_Factor, data = GTL)
+  summary(anova)
+  
+  # Poin C
+  data_summary <- group_by(GTL, Glass, Temp) %>%
+    summarise(mean = mean(Light), sd = sd(Light)) %>%
+    arrange(desc(mean))
+  print(data_summary)
